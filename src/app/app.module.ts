1dbed8dea7a3e02770db33f461a805e4f3ac7ee8
adm <etomojapo4ta@gmail.com>
2019-02-12 21:03:56 +0200

    remove test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServicesModule } from './services/services.module';
 import { ComponentsModule } from './components/components.module';
-import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
